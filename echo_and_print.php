695b73f874482e6a995e7aa449cf2060d3b32b87
Vikash Viraj <vikashviraj3@gmail.com>
2020-04-19 08:34:10 +0530

    PHP Echo and Print method

---
+++ echo_and_print.php
@@ -0,0 +1,14 @@
+<?php
+
+    echo "Hello, PHP <br>";
+    echo("Hey PHP <br>");
+
+    print "Hello, Print <br>";
+    print("Hey, Print <br>");
+
+    $n = 10;
+    echo "value of n is:".$n." using echo <br>";
+
+    print "Value of n is:".$n;
+
+?>
